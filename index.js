6617c0cdd6da234a418b90c90155f43e4ab8f94c
Pixtane <94226146+Pixtane@users.noreply.github.com>
2023-10-16 14:16:32 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,306 @@
+let game = document.querySelector(".game");
+let cells = document.querySelectorAll(".cell");
+let scoreEl = document.querySelector("#score");
+let cellsObj = [];
+let score = 0;
+for (let i=0; i<cells.length; i++) {cellsObj.push(0);}
+
+let colors = {
+    0: {background: "#CCC0B3", foreground: "#776E65"},
+    2: {background: "#EEE4DA", foreground: "#776E65"},
+    4: {background: "#EDE0C8", foreground: "#776E65"},
+    8: {background: "#F2B179", foreground: "white"},
+    16: {background: "#F59563", foreground: "white"},
+    32: {background: "#F67C5F", foreground: "white"},
+    64: {background: "#F65E3B", foreground: "white"},
+    128: {background: "#EDCF72", foreground: "#776E65"},
+    256: {background: "#EDCC62", foreground: "#776E65"},
+    512: {background: "#EDC850", foreground: "#776E65"},
+    1024: {background: "#3C3A32", foreground: "#776E65"},
+    2048: {background: "#3C3A32", foreground: "#776E65"},
+    4096: {background: "#3F3932", foreground: "#776E65"},
+};
+
+function updateBoard() {
+    if (scoreEl.innerHTML !== score)
+    {
+        scoreEl.innerHTML = score;
+
+    }
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].innerHTML = cellsObj[i] === 0 ? "" : cellsObj[i];
+        cells[i].style.backgroundColor = colors[cellsObj[i]].background;
+        cells[i].style.color = colors[cellsObj[i]].foreground;
+    }
+}
+
+
+function generateNewCell() {
+    let emptyCellIndices = [];
+    
+    for (let i = 0; i < cellsObj.length; i++) {
+        if (cellsObj[i] === 0) {
+            emptyCellIndices.push(i);
+        }
+    }
+
+    if (emptyCellIndices.length === 0) {
+        // Handle game over condition here
+        console.log("Game over!");
+        return;
+    }
+
+    let randomIndex = Math.floor(Math.random() * emptyCellIndices.length);
+    let cellGenerateIndex = emptyCellIndices[randomIndex];
+    
+    cellsObj[cellGenerateIndex] = 2;
+    updateBoard();
+}
+
+
+
+generateNewCell();
+generateNewCell();
+
+function isIInEveryNthElement(arr, i, n) {
+    for (let j = 0; j < arr.length; j += n) {
+        if (arr[j] === i) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function moveCells(direction) {
+    const gridSize = 4; // Assuming a 4x4 grid, change this if your grid size is different
+
+    // Define movement vectors based on the direction
+    const vectors = {
+        up: [-1, 0],
+        down: [1, 0],
+        left: [0, -1],
+        right: [0, 1],
+    };
+
+    // Iterate over each cell in the grid
+    if (direction === "up") {
+        for (let i = 0; i < cells.length; i++) {
+            const cell = cellsObj[i];
+            if (cell === 0) {
+                continue;
+            }
+            let tempIndex = i;
+            
+            if (i < gridSize) {
+                continue;
+            }
+            
+            console.log(tempIndex - gridSize)
+            //check if available empty space above
+            while (tempIndex - gridSize >= 0) {
+                if (cellsObj[tempIndex - gridSize] === 0) {
+                    cellsObj[tempIndex - gridSize] = cell;
+                    cellsObj[tempIndex] = 0;
+                    tempIndex -= gridSize;
+                } else if (cellsObj[tempIndex - gridSize] === cell) {
+                    cellsObj[tempIndex - gridSize] *= 2;
+                    cellsObj[tempIndex] = 0;
+                    tempIndex -= gridSize;
+                    score += cellsObj[tempIndex];
+                } 
+                else {
+                    break;
+                }
+            }
+
+
+            
+        }
+    }
+    // Iterate over each cell in the grid
+    if (direction === "down") {
+        for (let i = cells.length; i >= 0; i--) {
+            const cell = cellsObj[i];
+            if (cell === 0) {
+                continue;
+            }
+            let tempIndex = i;
+            
+            if (i >= gridSize*(gridSize-1)) {
+                continue;
+            }
+            
+            //check if available empty space above
+            while (tempIndex + gridSize <= gridSize*gridSize) {
+                console.log("sdhu");
+                if (cellsObj[tempIndex + gridSize] === 0) {
+                    cellsObj[tempIndex + gridSize] = cell;
+                    cellsObj[tempIndex] = 0;
+                    tempIndex += gridSize;
+                } else if (cellsObj[tempIndex + gridSize] === cell) {
+                    cellsObj[tempIndex + gridSize] *= 2;
+                    cellsObj[tempIndex] = 0;
+                    tempIndex += gridSize;
+                    score += cellsObj[tempIndex];
+                } 
+                else {
+                    break;
+                }
+            }
+
+
+            
+        }
+    }
+    // Iterate over each cell in the grid
+    if (direction === "left") {
+        let listOfIndexes = [];
+        for (let i = 0; i < gridSize; i++)
+        {
+            for (let j = 0; j < gridSize; j++)
+            {
+                listOfIndexes.push(i+gridSize*j)
+            }
+        }
+        for (let i in listOfIndexes) {
+            const cell = cellsObj[i];
+            if (cell === 0) {
+                continue;
+            }
+            let tempIndex = i;
+            
+            if (i % gridSize === 0) {
+                console.log("skipped");
+                continue;
+            }
+            
+            //check if available empty space to the left
+            while (tempIndex % gridSize !== 0) {
+                console.log("sdhu");
+                if (cellsObj[tempIndex - 1] === 0) {
+                    cellsObj[tempIndex - 1] = cell;
+                    cellsObj[tempIndex] = 0;
+                    tempIndex--;
+                } else if (cellsObj[tempIndex - 1] === cell) {
+                    cellsObj[tempIndex - 1] *= 2;
+                    cellsObj[tempIndex] = 0;
+                    tempIndex--;
+                    score += cellsObj[tempIndex];
+                } 
+                else {
+                    break;
+                }
+            }
+
+
+            
+        }
+    }
+
+    // Iterate over each cell in the grid
+    if (direction === "right") {
+        let listOfIndexes = [];
+        for (let i = gridSize; i > 0; i--)
+        {
+            for (let j = 0; j < gridSize; j++)
+            {
+                listOfIndexes.push(i-1+gridSize*j)
+            }
+        }
+        console.log(listOfIndexes);
+        for (let i of listOfIndexes) {
+            const cell = cellsObj[i];
+            if (cell === 0) {
+                continue;
+            }
+            let tempIndex = i;
+            
+            if ((i+1) % gridSize === 0) {
+                console.log("skipped");
+                continue;
+            }
+            
+            //check if available empty space to the left
+            while ((tempIndex+1) % gridSize !== 0) {
+                console.log("sdhu");
+                console.log(tempIndex, Number(tempIndex)+1, cellsObj[Number(tempIndex)+1]);
+                if (cellsObj[Number(tempIndex) + 1] === 0) {
+                    console.log("mvdright");
+                    cellsObj[tempIndex + 1] = cell;
+                    cellsObj[tempIndex] = 0;
+                    tempIndex++;
+                } else if (cellsObj[Number(tempIndex) + 1] === cell) {
+                    cellsObj[tempIndex + 1] *= 2;
+                    cellsObj[tempIndex] = 0;
+                    tempIndex++;
+                    score += cellsObj[tempIndex];
+                } 
+                else {
+                    break;
+                }
+            }
+
+
+            
+        }
+    }
+
+    updateBoard();
+}
+
+let keydownCheck = false;
+
+document.addEventListener("keydown", (e) => {
+    if (keydownCheck) return;
+    keydownCheck = true;
+    if (e.key === "ArrowUp") {
+        moveCells("up");
+        generateNewCell();
+    }
+    if (e.key === "ArrowDown") {
+        moveCells("down");
+        generateNewCell();
+    }
+    if (e.key === "ArrowLeft") {
+        moveCells("left");
+        generateNewCell();
+    }
+    if (e.key === "ArrowRight") {
+        moveCells("right");
+        generateNewCell();
+    }
+})
+
+document.addEventListener("keyup", (e) => {
+    keydownCheck = false
+})
+
+// Remove animation classes when the animation ends
+cells.forEach((cell) => {
+    cell.addEventListener("animationend", () => {
+      cell.classList.remove("slide-up", "slide-down", "slide-left", "slide-right");
+    });
+  });
+  
+
+// Function to update cell size based on the game container size
+function updateCellSize() {
+    console.log("resize")
+    const cellSize = (game.offsetWidth / 4) - 20; // Adjust the margin as needed
+
+    cells.forEach(cell => {
+        cell.style.borderRadius = cellSize / 5 + 'px';
+        cell.style.fontSize = (cellSize * 0.4) + 'px'; // Adjust the font size as needed
+    });
+
+    game.style.gap = cellSize/8 + 'px';
+    game.style.padding = cellSize/8 + 'px';
+    game.style.borderRadius = cellSize / 5 + 'px';
+
+    const score = document.querySelector('.score');
+    score.style.fontSize = (cellSize * 0.3) + 'px';
+}
+
+// Update cell size initially and on window resize
+window.addEventListener('resize', updateCellSize);
+window.addEventListener('load', updateCellSize);
